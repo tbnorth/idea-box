dd044ec77dcbf400cf0ad2d50856886aa383e951
Tim Jarzombek <tim@jarz.net>
2016-09-17 16:26:19 -0400

    remove comments from example_urls

--- idea/example_urls.py
+++ idea/example_urls.py
@@ -5,7 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
    url(r'^admin/', include(admin.site.urls)),
-   url(r'^comments/', include('django.contrib.comments.urls'))
 )
 
 urlpatterns.append(url(r'', include('idea.urls', namespace="idea")))

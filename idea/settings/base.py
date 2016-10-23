b880b5eeda9ccc23209509946049b66edd7496de
Tim Jarzombek <tim@jarz.net>
2016-10-23 11:33:46 -0400

    uncomment some social items

--- idea/settings/base.py
+++ idea/settings/base.py
@@ -161,8 +161,8 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 # `allauth` needs this from django
                 'django.template.context_processors.request',
-                #'allauth.account.context_processors.account',
-                #'allauth.socialaccount.context_processors.socialaccount',
+                'allauth.account.context_processors.account',
+                'allauth.socialaccount.context_processors.socialaccount',
             ],
             'debug': DEBUG
         },
